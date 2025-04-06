7b1858f51d30b11d862e833c55006f4cc57c3eb2
yez <yez1125lin@gmail.com>
2025-04-06 20:02:21 +0800

    implement login and createUser

--- server.py
+++ server.py
@@ -1,13 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 import motor.motor_asyncio
 import os
+from pydantic import BaseModel
+import bcrypt
 
-
+class user_info(BaseModel):
+    account:str
+    password:str
 
 app = FastAPI()
 
 client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017/")
 db = client[os.getenv("DATABASE_URL")]
+user_collection = db["user"]
 collection = db["plc"]
 
 @app.get("/")
@@ -15,3 +20,26 @@ async def DB_test():
     mytestData = { "temp": 25, "humidity": "78%" }
     await collection.insert_one(mytestData)
     return "Success"
+
+@app.post("/createUser")
+async def create_user(user:user_info):
+    
+    result = {"account":user.account,"password":bcrypt.hashpw(user.password.encode("utf-8"), bcrypt.gensalt())}
+    await user_collection.insert_one(result)
+
+    return {"message": "新增成功"}
+
+
+@app.post("/login")
+async def login(user:user_info):
+    user_in_db = await user_collection.find_one({"account": user.account.strip()})
+    
+    if not user_in_db:
+        raise HTTPException(status_code=404, detail="使用者不存在")
+
+    # 檢查密碼
+    if not bcrypt.checkpw(user.password.encode("utf-8"), user_in_db["password"]):
+        raise HTTPException(status_code=401, detail="密碼錯誤")
+
+    return {"message": "登入成功"}
+    

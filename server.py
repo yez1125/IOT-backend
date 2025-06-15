f3bed073d1045286d919d022110a12f5d6b2e155
yez <yez1125lin@gmail.com>
2025-06-15 19:05:19 +0800

    add  modify function and get user function

--- server.py
+++ server.py
@@ -9,21 +9,36 @@ from datetime import datetime
 import json
 from fastapi.middleware.cors import CORSMiddleware
 
-
-
-
 #定義物件
 class user_info(BaseModel):
     account:str
     password:str
     func_permissions:list[str]
+    company:str
 
 class login_info(BaseModel):
     account:str
     password:str
-    
+
+class modified_info(BaseModel):
+    account:str
+    func_permissions:list[str]
+
+class abox_info(BaseModel):
+    CO:float
+    CO2:float
+    toxic_gas:float
+    temperature:float
+    humidity:float
+
+class fan_info(BaseModel):
+    rpm:int
+
+class fridge_info(BaseModel):
+    temperature:float    
 
 app = FastAPI()
+
 #CORS
 origins = [
     "*"
@@ -60,14 +75,17 @@ async def protected_route(user=Depends(get_current_user)):
 
 @app.post("/api/createUser")
 async def create_user(user:user_info,auth=Depends(get_current_user)):
-
     account = await user_collection.find_one({"account": auth["account"]})
-    
+    print(user.account)
+
     if not "superuser" in account["func_permissions"]:
             raise HTTPException(status_code=401, detail="權限不足")
-    result = {"account":user.account,"password":bcrypt.hashpw(user.password.encode("utf-8"), bcrypt.gensalt()),"func_permissions":user.func_permissions}
+    
+    if await user_collection.find_one({"account": user.account}):
+            raise HTTPException(status_code=401, detail="帳號已存在")
+    
+    result = {"account":user.account,"password":bcrypt.hashpw(user.password.encode("utf-8"), bcrypt.gensalt()),"func_permissions":user.func_permissions,"company":user.company}
     await user_collection.insert_one(result)
-
     return {"message": "新增成功"}
 
 @app.post("/api/login")
@@ -83,8 +101,47 @@ async def login(user:login_info):
     
     # 產生 JWT token
     token = create_access_token({"account": user.account})
-    
     return {"access_token": token,"permissions":user_in_db["func_permissions"]}
+
+@app.post("/api/modifyPermissions")
+async def modify_permissions(info:modified_info,auth=Depends(get_current_user)):
+    account = await user_collection.find_one({"account": auth["account"]})
+    print("modify_user" in account["func_permissions"])
+
+    if not "superuser" in account["func_permissions"] and not "modify_user" in account["func_permissions"]:
+            raise HTTPException(status_code=401, detail="權限不足")
+    
+    await user_collection.update_one({"account":info.account},{"$set":{"func_permissions":info.func_permissions}})
+    
+    return {"message": "新增成功"}
+
+@app.get("/api/getUsers")
+async def get_users(auth=Depends(get_current_user)):
+    account = await user_collection.find_one({"account": auth["account"]})
+    result = []
+
+    if not "superuser" in account["func_permissions"] and not "get_users" in account["func_permissions"]:
+            raise HTTPException(status_code=401, detail="權限不足")
+    
+    if "superuser" in account["func_permissions"]:
+        data = user_collection.find()
+        async for person in data:
+            if "superuser" in person["func_permissions"]:
+                 continue
+            personData = {"account":person["account"],"func_permissions":person["func_permissions"],"company":person["company"]}
+            result.append(personData)
+            
+        return result
+    
+    if "get_users" in account["func_permissions"]:
+        data = user_collection.find({"company":account["company"]})
+        async for person in data:
+            personData = {"account":person["account"],"func_permissions":person["func_permissions"],"company":person["company"]}
+            result.append(personData)
+            
+        return result
+        
+    
 
 @app.websocket("/ws/{machine}")
 async def websocket_endpoint(websocket: WebSocket,machine:str):

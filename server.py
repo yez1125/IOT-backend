bb9904b4c7d12fcb9a0c3d0af105bec8d463797d
yezlin <yez1125lin@gmail.com>
2025-12-07 16:02:25 +0800

    fix ip address

--- server.py
+++ server.py
@@ -829,7 +829,7 @@ async def turn_on(target:machine_company,auth=Depends(get_current_user)):
         raise HTTPException(status_code=401, detail="公司不一致")
     
     company = await company_collection.find_one({"company":target.company})
-    address = "http://" + company["IP"] + "/on"
+    address = company["IP"] + "/on"
     async with httpx.AsyncClient() as client:
         response = await client.post(address)
         return response.json()
